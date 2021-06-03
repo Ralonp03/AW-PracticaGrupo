89061bfb1a46453c7d9f134d565d78a6b0c960ce
Ricardo Alonso <47599651+Ralonp03@users.noreply.github.com>
2021-06-03 21:00:56 +0200

    Update BuyCard.vue
    Vista Comprar Carta acabada

--- client/src/views/BuyCard.vue
+++ client/src/views/BuyCard.vue
@@ -1,100 +1,108 @@
-
-
 <template>
+  <div class="App">
+    <TaskBar />
+    <div class="ml-25">
+      <div
+        class="min-h-screen bg-gray-300 dark:bg-gray-900 py-6 flex flex-col justify-center sm:py-s"
+      >
+        <div
+          class="ml-140 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 px-2"
+        >
+          <div></div>
 
-<div class="app">
+          <div
+            class="bg-gray-100 border-yellow-600 dark:bg-gray-800 bg-opacity-95 border-opacity-60 | p-30 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-yellow-400 dark:hover:bg-yellow-600 hover:border-transparent | transition-colors duration-500"
+          >
+            <img v-bind:src="require('../assets/Coches/c5.png')" alt="" />
+          </div>
+          <!-- END SMALL CARD ROUNDED -->
 
-  <TaskBar/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cerulean/bootstrap.min.css" integrity="sha384-3fdgwJw17Bi87e1QQ4fsLn4rUFqWw//KU0g8TvV6quvahISRewev6/EocKNuJmEw" crossorigin="anonymous">  <div class="Background">
-
-    <div class="container">
-      <div class="flexContainer">
-        <div class="card mt-4">
-          
-            <div class="card-body" >
-                <h4 class="card-title" style="text-align:center">Carta</h4>
-                    <img v-bind:src="require('../assets/Coches/c5.png')" alt="">
+          <!-- SMALL CARD ROUNDED -->
+          <div
+            class="bg-gray-100 border-green-600 dark:bg-gray-800 bg-opacity-95 border-opacity-60 | p-4 border-solid rounded-3xl border-2 | "
+          >
+            <h4
+              class="text-black dark:text-gray-100 text-justify font-semibold"
+            >
+              Precio:
+            </h4>
+            <span class="datas">{{ priceCard }}</span
+            ><br />
+            <h4
+              class="text-black dark:text-gray-100 text-justify font-semibold"
+            >
+              Mis puntos:
+            </h4>
+            <span class="datas">{{ myPoints }}</span
+            ><br />
+            <div class="flex justify-center m-6">
+              <div>
+                <button
+                  v-on:click.prevent="decrement"
+                  class="bg-blue-500 rounded-full font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 m-6"
+                >
+                  -
+                </button>
+              </div>
+              <div>
+                <span class=" line-height-username3 text-5xl">{{ count }}</span>
+              </div>
+              <div>
+                <button
+                  v-on:click.prevent="increment"
+                  class="bg-blue-500 rounded-full font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 m-6"
+                >
+                  +</button
+                ><br />
+              </div>
             </div>
+
+            <div class="flex flex-col justify-center">
+              <button
+                v-on:click.prevent="buy"
+                class="bg-blue-500 rounded-full font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600"
+              >
+                Comprar
+              </button>
+            </div>
+          </div>
+          <!-- END SMALL CARD ROUNDED -->
         </div>
-        <div class="card mt-4" >
-            <div class="card-body">
-                <h4 class="card-title">Precio: </h4><span class="datas">{{ priceCard }}</span><br>
-                <h4 class="card-title">Mis puntos: </h4><span class="datas">{{ myPoints }}</span><br>
-                <div class="card-text mb-2">
-            </div>
-        <button v-on:click.prevent="decrement"  class="btn btn-success" style="margin:20px;">-</button><span>{{ count }}</span>
-        <button v-on:click.prevent="increment"  class="btn btn-success"  style="margin:20px">+</button><br>
-        <div class="col text-center">
-        <button v-on:click.prevent="buy"  class="btn btn-success"  style="text-align:center">Comprar</button>
-               </div>
-               </div>
-           </div>
-         </div>
-   </div>
-
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import TaskBar from "../views/TaskBar.vue"
+import TaskBar from "../views/TaskBar.vue";
 export default {
-  name: 'Gallery',
+  name: "Gallery",
   components: {
-    TaskBar
+    TaskBar,
   },
-  data: ()=> {
+  data: () => {
     return {
       count: 0,
-      priceCard : 200,
-      myPoints : 400,
-    }
+      priceCard: 200,
+      myPoints: 400,
+    };
   },
   methods: {
-    increment () {
+    increment() {
       this.count++;
     },
-    decrement () {
-      if(this.count > 0){
-        this.count-- ;
+    decrement() {
+      if (this.count > 0) {
+        this.count--;
       }
-    },buy(){
-          if(this.myPoints >= (this.priceCard * this.count)){
-       this.myPoints = this.myPoints  - this.count * this.priceCard;
-       }
-      
     },
-  }
-}
+    buy() {
+      if (this.myPoints >= this.priceCard * this.count) {
+        this.myPoints = this.myPoints - this.count * this.priceCard;
+      }
+    },
+  },
+};
 </script>
 
-<style>
-
-.button {
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.flexContainer {
-  height: 100%;  
-  display: flex;
-  flex-direction: row;
-}
-
-  .flexContainer > div {
-  background-color: #f1f1f1;
-  margin: 150px;
-  max-height: 100%;
-  padding: 20px;
-  font-size: 30px;
-}
-
-</style>
+<style></style>
