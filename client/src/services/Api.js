d35619531cc7eb649ed17a518a520cf0859bd299
agonzc32 <47741999+agonzc32@users.noreply.github.com>
2021-06-13 20:27:36 +0200

    Add files via upload

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -81,6 +81,18 @@ const loginON = async(name) => {
     return response
 }
 
+const compruebaEvento = async(question,answer) => {
+    console.log("paso1");
+    const params = {
+        question,
+        answer
+    }
 
 
-module.exports = { loginUser, registerUser , updateDatas, consultaPrueba, deleteUnitsOfCard, recopilar , getCard, loginON}
+    const response = await axios.post('http://localhost:8081/api/compruebaPregunta', params)
+    return response
+}
+
+
+
+module.exports = { loginUser, registerUser , updateDatas, consultaPrueba, deleteUnitsOfCard, recopilar , getCard, loginON, compruebaEvento}
