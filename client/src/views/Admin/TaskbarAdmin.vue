31f2884f68cb21abfaccfb026b790e91c73bc86b
albert <acanoa01@estudiantes.unileon.es>
2021-06-15 12:58:11 +0200

    taskbar admin actualizada

--- client/src/views/Admin/TaskbarAdmin.vue
+++ client/src/views/Admin/TaskbarAdmin.vue
@@ -1,14 +1,68 @@
 <template>
-  <div class="h-screen w-60 bg-blue-600 flex flex-col text-center text-white items-center border-2 border-black">
+  <div
+    class="
+      h-screen
+      w-60
+      bg-blue-600
+      flex flex-col
+      text-center text-white
+      items-center
+      border-2 border-black
+    "
+  >
     <h2 class="font-serif mb-4">KioskoROC</h2>
     <div class="logo mb-4">
       <img src="../../assets/admin.png" class="ml-4 h-24 w-24" />
     </div>
-    <p class="mb-4">Administrador: <span class="font-bold w-full "> {{ username }}</span></p>
-    <div class="buttons flex flex-col w-full ">
-      <button @click="colectionRoute" class="block hover:bg-blue-700 hover:border-2">Colecciones</button>
-      <button @click="cardRoute" class="mt-4 block hover:bg-blue-700 hover:border-4 hover:border-gray-200">Cromos</button>
-      <button @click="inicioRoute" class="mt-4 block hover:bg-blue-700 hover:border-4 hover:border-gray-200">Inicio</button>
+    <p class="mb-4">
+      Administrador: <span class="font-bold w-full"> {{ username }}</span>
+    </p>
+    <div class="buttons flex flex-col w-full items-center">
+      <button
+        @click="colectionRoute"
+        class="block w-full hover:bg-blue-700 hover:border-2"
+      >
+        Colecciones
+      </button>
+      <button
+        @click="cardRoute"
+        class="
+          mt-4
+          w-full
+          block
+          hover:bg-blue-700
+          hover:border-4 hover:border-gray-200
+        "
+      >
+        Cromos
+      </button>
+      <button
+        @click="inicioRoute"
+        class="
+          mt-4
+          block
+          w-full
+          hover:bg-blue-700
+          hover:border-4 hover:border-gray-200
+        "
+      >
+        Inicio
+      </button>
+      <button
+        @click="logout"
+        class="
+          logoutBtn
+          w-56
+          text-center text-black
+          bg-gray-200
+          absolute
+          bottom-4
+          border-4 border-red-600
+          rounded-full
+        "
+      >
+        Cerrar Sesión
+      </button>
     </div>
   </div>
 </template>
@@ -44,11 +98,14 @@ export default {
       username,
       colectionRoute,
       cardRoute,
-      inicioRoute
+      inicioRoute,
     };
   },
 };
 </script>
 
 <style>
+.logoutBtn:hover {
+  transform:scale(0.95)
+}
 </style>
